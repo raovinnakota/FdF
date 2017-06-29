d1a69b0aba67a51735d08784c89a02f806fb5d16
raovinnakota <eswarv@sbcglobal.net>
2017-06-28 17:03:43 -0700

    Adding main.c

--- main.c
+++ main.c
@@ -1,12 +1,33 @@
 #include "minilibx_macos/mlx.h"
+#include <stdio.h>
+
+int		my_key_funct(int keycode, void *param)
+{
+	printf("key event %d\n", keycode);
+	mlx_pixel_put(mlx, win, 300, 300, 0xFF00FF);
+	return (0);
+}
 
 int		main()
 {
 	void	*mlx;
 	void	*win;
+	int		x;
+	int		y;
 
 	mlx = mlx_init();
-	win = mlx_new_window(mlx, 420, 420, "mlx 42");
+	win = mlx_new_window(mlx, 400, 400, "mlx 42");
+	y = 50;
+	while (y < 150)
+	{
+		x = 50;
+		while (x < 150)
+		{
+			mlx_pixel_put(mlx, win, x, y, 0x00FFFFFF);
+			x++;
+		}
+		y++;
+	}
+	mlx_key_hook(win, my_key_funct, 0);
 	mlx_loop(mlx);
-	return (0);
 }
