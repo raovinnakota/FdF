e599becfefdc1e829a22997aa2d5e27c427e1f29
raovinnakota <eswarv@sbcglobal.net>
2017-07-17 13:51:11 -0700

    updating'

--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: rvinnako <rvinnako@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/28 17:18:31 by rvinnako          #+#    #+#             */
-/*   Updated: 2017/07/03 15:42:02 by rvinnako         ###   ########.fr       */
+/*   Updated: 2017/07/16 15:50:38 by rvinnako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,22 @@ typedef struct		s_line
 	int				y_end;
 }					t_line;
 
+typdef struct		s_env
+{
+	int				map_x;
+	int				map_y;
+	int				map_area;
+	int				scale;
+	int				h_min;
+	int				h_max;
+}					t_env;
+
+tydef struct		s_point
+{
+	int				x;
+	int				y;
+	int				z;
+}					t_point;
 
 /*----parse.c----*/
 char	**fill_map(int fd);
