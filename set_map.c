d8de8f270989bfe5c956cb10975c3698ecac131f
raovinnakota <eswarv@sbcglobal.net>
2017-07-20 20:41:14 -0700

    Updated to fill map struct

--- set_map.c
+++ set_map.c
@@ -6,7 +6,7 @@
 /*   By: rvinnako <rvinnako@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/20 12:22:15 by rvinnako          #+#    #+#             */
-/*   Updated: 2017/07/20 17:43:30 by rvinnako         ###   ########.fr       */
+/*   Updated: 2017/07/20 20:41:03 by rvinnako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,21 @@ char	**get_row(char *str)
 	return (row);
 }
 
+void 	set_line(int *int_arr, char **row)
+{
+	int		c_count;
+	int		i_count;
+
+	c_count = 0;
+	i_count = 0;
+	while (row[c_count])
+	{
+		int_arr[i_count] = ft_atoi(row[c_count]);
+		i_count++;
+		c_count++;
+	}
+}
+
 void	set_dimensions(t_map *map, char **arr)
 {
 	int height;
@@ -27,10 +42,27 @@ void	set_dimensions(t_map *map, char **arr)
 
 	width = ft_arrsize(get_row(arr[0]));
 	height = ft_arrsize(arr);
-	printf("width:%d\nheight:%d\n", width, height);
 	map->map_x = width;
 	map->map_y = height;
 	map->map_area = (map->map_x * map->map_y);
+}
+
+/*
+int		**get_int_map(char **map)
+{
+	int		i;
+	int		**ret;
+	char	**temp;
+
+	i = 0;
+	ret = (int**)malloc(sizeof(int*) * get_area(map));
+	while ((temp = get_row(map[i])))
+	{
+		ret[i] = (int*)malloc(sizeof(int) * ft_arrsize(temp));
+		set_line(ret[i], temp);
+		i++;
+	}
+	return (ret);
 }
 
 /*
